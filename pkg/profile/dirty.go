b26dd6fbb49914c37abd58dbb968c04b804ced11
Gleb Buzin <qufiwefefwoyn@gmail.com>
2022-02-25 21:12:56 +0300

    fix(run.sh): up the package, not an individual main file

---
+++ pkg/profile/dirty.go
@@ -0,0 +1,14 @@
+package profile
+
+import (
+	"time"
+
+	"go.uber.org/zap"
+)
+
+// To use: defer profile.Duration(time.Now(), "IntFactorial")
+func Duration(lg *zap.Logger, invocation time.Time, name string) {
+	elapsed := time.Since(invocation)
+
+	lg.Info("dirty profile", zap.String("name", name), zap.Duration("elapsed", elapsed))
+}
