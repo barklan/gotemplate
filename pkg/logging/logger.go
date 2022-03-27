d2d1f003b09f24f57dad13893d0640f402c9135b
Gleb Buzin <qufiwefefwoyn@gmail.com>
2022-03-27 23:38:23 +0300

    ci(github): add pre-commit custom job
    Add manual action for pre-commit and fix logger package issues.

--- pkg/logging/logger.go
+++ pkg/logging/logger.go
@@ -2,6 +2,8 @@
 package logging
 
 import (
+	"fmt"
+
 	"github.com/barklan/gotemplate/pkg/system"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
@@ -28,9 +30,20 @@ func Dev() (*zap.Logger, error) {
 	zapConfig := zap.NewDevelopmentConfig()
 	zapConfig.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
 	zapConfig.EncoderConfig.TimeKey = ""
-	return zapConfig.Build()
+
+	logger, err := zapConfig.Build()
+	if err != nil {
+		return nil, fmt.Errorf("failed to construct dev logger: %w", err)
+	}
+
+	return logger, nil
 }
 
 func Prod() (*zap.Logger, error) {
-	return zap.NewProduction()
+	logger, err := zap.NewProduction()
+	if err != nil {
+		return nil, fmt.Errorf("failed to construct prod logger: %w", err)
+	}
+
+	return logger, nil
 }
