1eacd2f734463087dfaa8b7a4c23fcd674896e82
Gleb Buzin <qufiwefefwoyn@gmail.com>
2022-06-01 16:29:10 +0300

    style(scripts): reformat build_env.sh

--- scripts/build_env.sh
+++ scripts/build_env.sh
@@ -5,8 +5,7 @@ set -e
 
 export ENV_FOR="${ENV_FOR:-local}"
 
-if [ "${ENV_FOR}" != 'local' ]
-then
-    cp ./.env ./environment/base.env
-    sort -u -t '=' -k 1,1 "./environment/${ENV_FOR}.override.env" './environment/base.env' | grep -v '^$\|^\s*\#' > './.env'
+if [ "$ENV_FOR" != 'local' ]; then
+	cp ./.env ./environment/base.env
+	sort -u -t '=' -k 1,1 "./environment/$ENV_FOR.override.env" './environment/base.env' | grep -v '^$\|^\s*\#' >'./.env'
 fi
